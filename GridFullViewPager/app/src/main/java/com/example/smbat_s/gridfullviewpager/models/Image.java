686f2173e6173de1ba173a5aaea2a190ca82d6f3
Smbat Sargsyan <smbat_s@Mac-mini-MC-0001.local>
2017-06-22 11:56:47 +0400

    Added whole project.

---
+++ GridFullViewPager/app/src/main/java/com/example/smbat_s/gridfullviewpager/models/Image.java
@@ -0,0 +1,62 @@
+package com.example.smbat_s.gridfullviewpager.models;
+
+
+public class Image {
+
+    private String name;
+    private String small;
+    private String medium;
+    private String large;
+    private String timestamp;
+
+    public Image() {
+    }
+
+    public Image(String name, String small, String medium, String large, String timestamp) {
+        this.name = name;
+        this.small = small;
+        this.medium = medium;
+        this.large = large;
+        this.timestamp = timestamp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSmall() {
+        return small;
+    }
+
+    public void setSmall(String small) {
+        this.small = small;
+    }
+
+    public String getMedium() {
+        return medium;
+    }
+
+    public void setMedium(String medium) {
+        this.medium = medium;
+    }
+
+    public String getLarge() {
+        return large;
+    }
+
+    public void setLarge(String large) {
+        this.large = large;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
